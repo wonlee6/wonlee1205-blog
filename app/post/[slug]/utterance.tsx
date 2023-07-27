7772bd7cd0ef459cd174df87ae0bc844c9b94dc0
swlee <swlee@bizflow.com>
2023-07-27 18:06:06 +0900

    commit

--- app/post/[slug]/utterance.tsx
+++ app/post/[slug]/utterance.tsx
@@ -12,7 +12,7 @@ function Utterance(): JSX.Element {
         const scriptElment = document.createElement('script')
         scriptElment.src = 'https://utteranc.es/client.js'
         scriptElment.async = true
-        scriptElment.setAttribute('repo', 'wonlee6/wonlee6-blog')
+        scriptElment.setAttribute('repo', 'wonlee6/wonlee1205-blog')
         scriptElment.setAttribute('issue-term', 'pathname')
         scriptElment.setAttribute('theme', 'github-dark-orange')
         scriptElment.setAttribute('crossorigin', 'anonymous')
