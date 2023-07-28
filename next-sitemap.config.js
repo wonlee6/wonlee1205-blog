704c54b4cb65ab2488e964d80fefbec9feced94a
swlee <swlee@bizflow.com>
2023-07-28 18:53:03 +0900

    add next sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://wonlee6-blog.vercel.app/',
+  siteUrl: process.env.SITE_URL || 'https://wonlee1205-blog.vercel.app/',
   generateRobotsTxt: true // (optional)
   // ...other options
 }
