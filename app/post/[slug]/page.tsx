e096c57b85028415650a374881bc2a62dc843345
wonlee6 <wonlee6@gmail.com>
2023-09-15 22:06:15 +0900

    commit

--- app/post/[slug]/page.tsx
+++ app/post/[slug]/page.tsx
@@ -33,7 +33,7 @@ export default async function Page({
           <MarkdownViwer contentHtml={postData.contentHtml} />
         </div>
       )}
-      <Utterance />
+      {/* <Utterance /> */}
       <PostingGuide allPostsData={allPostsData} postData={postData} />
     </article>
   )
