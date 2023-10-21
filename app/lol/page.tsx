df6044cafd86169dd67b0d9f70ffd9b84d60ecc9
wonlee6 <wonlee6@gmail.com>
2023-10-22 02:54:48 +0900

    commit

--- app/lol/page.tsx
+++ app/lol/page.tsx
@@ -2,35 +2,53 @@ import {ChampInfo, ChampLotation} from '@/model/LOL.model'
 import LoLSearch from './search'
 import ChampRotation from './champRotation'
 
-export default async function LoLPage() {
-  const fetchChampion = await fetch(
+const fetchChampLotation = async () => {
+  if (
+    typeof process.env.NEXT_PUBLIC_RIOT_GAMES_BASE_URL === 'undefined' ||
+    typeof process.env.NEXT_PUBLIC_RIOT_GAMES_KEY === 'undefined'
+  )
+    return
+
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_RIOT_GAMES_BASE_URL}/lol/platform/v3/champion-rotations`,
+    {
+      method: 'GET',
+      headers: {
+        'X-Riot-Token': process.env.NEXT_PUBLIC_RIOT_GAMES_KEY
+      }
+    }
+  )
+  const data: ChampLotation = await response.json()
+  return data
+}
+
+const fetchChampionList = async () => {
+  const response = await fetch(
     'https://ddragon.leagueoflegends.com/cdn/13.20.1/data/ko_KR/champion.json',
     {
       method: 'GET'
     }
   )
-  const championData = await fetchChampion.json()
-  const champInfoList: ChampInfo[] = []
-  for (const key in championData.data) {
-    champInfoList.push(championData.data[key])
-  }
+  const data = await response.json()
+  return data
+}
+export default async function LoLPage() {
+  const getChampionData = fetchChampionList()
+  const getChampLotationData = fetchChampLotation()
 
-  const fetchChampLotations = await fetch(
-    `${
-      process.env.NEXT_PUBLIC_RIOT_GAMES_BASE_URL as string
-    }/lol/platform/v3/champion-rotations`,
-    {
-      method: 'GET',
-      headers: {
-        'X-Riot-Token': process.env.NEXT_PUBLIC_RIOT_GAMES_KEY as string
-      }
-    }
-  )
-  const champLotationData: ChampLotation = await fetchChampLotations.json()
+  const [champInfo, champLotation] = await Promise.all([
+    getChampionData,
+    getChampLotationData
+  ])
+
+  const champInfoList: ChampInfo[] = []
+  for (const key in champInfo.data) {
+    champInfoList.push(champInfo.data[key])
+  }
 
   const filteredChampLotationInfo = champInfoList.filter((item) => {
     let flag = false
-    champLotationData.freeChampionIds.forEach((ele) => {
+    champLotation?.freeChampionIds.forEach((ele) => {
       if (item.key === String(ele)) {
         flag = true
       }
